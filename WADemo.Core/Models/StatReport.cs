0112c3a2dceeafe7ea93c9cbe8f16140dd88cbca
Manav Misra <manavm@visionwebsoft.com>
2022-03-31 15:34:36 -0500

    Add StatReport Model

---
+++ WADemo.Core/Models/StatReport.cs
@@ -0,0 +1,18 @@
+﻿namespace WADemo.Core.Models;
+
+public class StatReport
+{
+  public decimal AvgHighTemp { get; set; }
+  public decimal MaxHighTemp { get; set; }
+  public decimal MinHighTemp { get; set; }
+
+  public decimal AvgLowTemp { get; set; }
+  public decimal MaxLowTemp { get; set; }
+  public decimal MinLowTemp { get; set; }
+
+  public decimal AvgHumidity { get; set; }
+  public decimal MaxHumidity { get; set; }
+  public decimal MinHumidity { get; set; }
+
+  // TODO: Override ToString() to return a string representation of the object (and same for WeatherRecord)
+}
